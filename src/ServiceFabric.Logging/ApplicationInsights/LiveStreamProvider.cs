a4a674137f8653d2776c6af660fd499b59c4e582
Peter Bons <github@2000miles.nl>
2017-09-19 16:08:39 +0200

    #3 Remove unneeded code and references

--- src/ServiceFabric.Logging/ApplicationInsights/LiveStreamProvider.cs
+++ src/ServiceFabric.Logging/ApplicationInsights/LiveStreamProvider.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Fabric;
-using Microsoft.ApplicationInsights.DependencyCollector;
+﻿using Microsoft.ApplicationInsights.DependencyCollector;
 using Microsoft.ApplicationInsights.Extensibility;
 using Microsoft.ApplicationInsights.Extensibility.PerfCounterCollector.QuickPulse;
 
@@ -17,14 +15,6 @@ namespace ServiceFabric.Logging.ApplicationInsights
 
         public void Enable()
         {
-            var profilerAssembliesPath = FabricRuntime.GetActivationContext().GetCodePackageObject("Code").Path;
-
-            Environment.SetEnvironmentVariable("COR_ENABLE_PROFILING", "1", EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("COR_PROFILER", "{324F817A-7420-4E6D-B3C1-143FBED6D855}", EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("COR_PROFILER_PATH", $@"{profilerAssembliesPath}\MicrosoftInstrumentationEngine_x86.dll", EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("MicrosoftInstrumentationEngine_Host", "{CA487940-57D2-10BF-11B2-A3AD5A13CBC0}", EnvironmentVariableTarget.Process);
-            Environment.SetEnvironmentVariable("MicrosoftInstrumentationEngine_HostPATH", $@"{profilerAssembliesPath}\Microsoft.ApplicationInsights.ExtensionsHost_x86.dll", EnvironmentVariableTarget.Process);
-
             var module = new DependencyTrackingTelemetryModule();
             module.Initialize(configuration);
 
